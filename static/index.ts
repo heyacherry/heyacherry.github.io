281a3f8092575804751303f3ab5e10cf155a880c
Cherry Liang <cherryliang@0xCheli.local>
2023-07-11 09:44:44 +1200

    feat: update links

--- static/index.ts
+++ static/index.ts
@@ -1,5 +1,5 @@
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/hellocherryliang/';
-export const GITHUB_URL = 'https://github.com/CaiYiLiang';
+export const GITHUB_URL = 'https://github.com/heyacherry';
 export const SUBQUERY_URL = 'https://subquery.network/';
 export const DOVETAIL_URL = 'https://palomagroup.com/';
 export const ASB_BANK_URL = 'https://www.asb.co.nz/about-us';
